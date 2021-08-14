62b182f229dc1db13f8f6a566958d056f4874dd0
선주 김 <tjswntjswn00@naver.com>
2021-08-14 16:06:18 +0900

    할 일 목록 & 삭제 버튼 마크업 작업하기

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
     <section>
         <ul>
-            <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{ todoItem }}</li>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
         </ul>
     </section>
 </template>
@@ -31,5 +37,32 @@
 </script>
 
 <style scoped>
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
 
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
