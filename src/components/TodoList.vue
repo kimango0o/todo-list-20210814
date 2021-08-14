0c7dbef09fc22101ec65b3965be19fb10d610ede
선주 김 <tjswntjswn00@naver.com>
2021-08-14 16:13:54 +0900

    선택한 할 일을 로컬 스토리지와 뷰 데이터에서 삭제하기

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -35,7 +35,13 @@
         },
         methods: {
             removeTodo(todoItem, index) {
-                console.log(todoItem, index);
+                // 로컬 스토리지의 데이터를 삭제
+                localStorage.removeItem(todoItem);
+                /*
+                    splice() : 배열의 특정 인덱스를 삭제하는 API
+                    할 일 삭제 처리
+                */
+                this.todoItems.splice(index, 1);
             }
         }
     }
