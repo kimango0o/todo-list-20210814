e028d3bb69ed02017721bbff651542fbc86d976c
선주 김 <tjswntjswn00@naver.com>
2021-08-14 13:06:50 +0900

    설명 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- script에서 설정한 컴포넌트 태그 추가 -->
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
     <TodoList></TodoList>
@@ -8,13 +9,20 @@
 </template>
 
 <script>
+  /*
+    특정 컴포넌트에서 다른 위치에 있는 컴포넌트의 내용을 불러오는 방법
+    import 불러온 파일의 내용이 담길 객체 from '불러올 파일 위치'
+      - 경로 헷갈리지 말고 정확하게 입력하기
+  */
   import TodoHeader from './components/TodoHeader.vue'
   import TodoInput from './components/TodoInput.vue'
   import TodoList from './components/TodoList.vue'
   import TodoFooter from './components/TodoFooter.vue'
 
   export default {
+    // 지역 컴포넌트 등록
     components: {
+      // template 에서 태그로 사용할 이름 : import 에서 설정한 객체
       'TodoHeader': TodoHeader,
       'TodoInput': TodoInput,
       'TodoList': TodoList,
