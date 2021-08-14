7d7f3b1a4acda570a18fdd6842a3c0c2485a53e9
선주 김 <tjswntjswn00@naver.com>
2021-08-14 13:08:45 +0900

    애플리케이션 제목 추가하기

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,13 @@
+<!--
+    TodoHeader.vue
+     - 사용 용도 : 애플리케이션 이름 표시
+-->
+
 <template>
-    <div>header</div>
+    <!-- [ 애플리케이션 제목 추가하기 ] -->
+    <header>
+        <h1>TODO LIST</h1>
+    </header>
 </template>
 
 <script>
