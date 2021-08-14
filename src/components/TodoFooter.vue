0788144a2a1c0235a6b5a3d1a099ea5e1f5ed077
선주 김 <tjswntjswn00@naver.com>
2021-08-14 16:18:35 +0900

    모두 삭제하기 버튼 추가하기

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>footer</div>
+    <div class="clearAllContainer shadow">
+        <span class="clearAllBtn" @click="clearTodo">
+            Clear All
+        </span>
+    </div>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            // 로컬 스토리지의 데이터를 모두 삭제
+            cleatTodo() {
+                localStorage.clear();
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
 
+    .clearAllBtn {
+        color: red;
+        display: block;
+    }
 </style>
