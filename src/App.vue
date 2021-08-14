3cd68406b6540a6a72cccb1b0b93f75cf28b121c
선주 김 <tjswntjswn00@naver.com>
2021-08-14 16:31:21 +0900

    TodoList에서 불필요한 코드 제거하기

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,19 @@
         todoItems: []
       }
     },
+    created() {
+      // localStorage에 저장된 데이터가 한 개라도 있는 경우
+      if (localStorage.length > 0) {
+          /*
+              localStorage에 저장된 모든 아이템을 한 번에 불러오는
+              API는 없기 때문에 반복문으로 아이템을 모두 불러와야 한다.
+          */
+          for (var i = 0; i < localStorage.length; i++) {
+              // push() : 배열의 끝 요소에 배열 아이템을 하나씩 추가하는 자바스크립트 내장 API
+              this.todoItems.push(localStorage.key(i));
+          }
+      }
+    },
     methods: {
       addTodo(todoItem) {
         /*
