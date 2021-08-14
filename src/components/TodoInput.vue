8e5235beeb921ce60eac4727d59b070cee533509
선주 김 <tjswntjswn00@naver.com>
2021-08-14 13:23:47 +0900

    addTodo() 안에 예외 처리 코드 넣기

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -14,12 +14,23 @@
         },
         methods: {
             addTodo() {
-                /*
-                    [ Application → Local Storage → http://localhost:8080 ]
-                     - Storage.setItem(key: string, value: string)
-                     : 왼 쪽에 key, 오른쪽에 value 저장
-                */
-                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                // Input 박스의 입력 값이 있을 때만 저장하기
+                if (this.newTodoItem !== "") {
+                    // Input 박스에 입력된 텍스트의 앞 뒤 공백 문자열 제거
+                    var value = this.newTodoItem && this.newTodoItem.trim();
+                    /*
+                        [ Application → Local Storage → http://localhost:8080 ]
+                        - Storage.setItem(key: string, value: string)
+                         : 왼 쪽에 key, 오른쪽에 value 저장
+                    */
+                    localStorage.setItem(value, value);
+                    // Input 박스의 입력 값을 초기화
+                    this.clearInput();
+                }
+            },
+            // Input 박스의 입력 값을 초기화 하기 위해 생성한 함수
+            clearInput() {
+                this.newTodoItem = '';
             }
         }
     }
